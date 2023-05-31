4c3426a04d22f0a22fff15d2c50b80e32397ec28
ruslanlap <106077551+ruslanlap@users.noreply.github.com>
2023-05-31 11:32:56 +0000

    cd

--- variables.tf
+++ variables.tf
@@ -5,19 +5,19 @@ variable "GOOGLE_PROJECT" {
 
 variable "GOOGLE_REGION" {
   type        = string
-  default     = "us-central1-c"
+  default     = "us-west1-b"  # Change to the desired region "us-west1-b"
   description = "GCP region to use"
 }
 
 variable "GKE_MACHINE_TYPE" {
   type        = string
-  default     = "g1-small"
+  default     = "e2-small"  # Change to the desired machine type "e2-small"
   description = "Machine type"
 }
 
 variable "GKE_NUM_NODES" {
   type        = number
-  default     = 2
+  default     = 1  # Change to the desired number of nodes 1
   description = "GKE nodes number"
 }
 
